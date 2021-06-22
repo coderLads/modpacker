d0eae63b566209836a67aa6e638676a61b9e6786
PanDoes <public@daltonlange.com>
2021-06-21 21:37:43 -0700

    Added tallyTheVotes and Hajimete *version* commands.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 /* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+import { download } from './util';
+
 const fs = require('fs');
 const Discord = require('discord.js');
 
@@ -19,7 +22,8 @@ client.on('message', (msg) => {
     if (msg.content[0] === '!'
        && msg.member.roles.cache.has('847891487597527050')) {
       if (msg.content === '!tallyTheVotes') tallyTheVotes(msg);
-      else if (msg.content === '!hajimete') return; // Start
+      else if (msg.content.split(' ')[0] === '!hajimete') hajimete(msg); // Start
+      else handleLink(msg);
       return;
     }
 
@@ -31,20 +35,38 @@ client.on('message', (msg) => {
 
 client.login(keys.token);
 
-function tallyTheVotes(msg: any): null {
+function hajimete(msg: any): void {
+  const version = msg.content.split(' ')[1];
   msg.channel.messages.fetch({ limit: 100 }).then((messages) => {
+    let deletionPromises = [];
     messages.forEach((message) => {
-      if (message.content === '!tallyTheVotes') return;
-      const reactions = message.reactions.cache;
-      const thumbsUp = reactions.find((reaction) => reaction.emoji.name === '👍');
-      const thumbsDown = reactions.find((reaction) => reaction.emoji.name === '👎');
-      console.log(message.content);
-      console.log(thumbsUp.count - thumbsDown.count);
+      deletionPromises.push(message.delete());
+    })
+    Promise.all(deletionPromises).then(() => {
+      msg.channel.send(`Voting opening for a ${version} server.
+        Please suggest mods for this version below by sending the curseforge
+        link. E.x: "https://www.curseforge.com/minecraft/mc-mods/jei"`);
     });
   });
 }
 
-function handleLink(msg: any): null {
+function tallyTheVotes(msg: any): void {
+  msg.channel.messages.fetch({ limit: 100 }).then((messages) => {
+    messages.forEach((message) => {
+      if (message.content.startsWith("Voting")) return;
+      if (message.content === '!tallyTheVotes') return;
+      const reactions = message.reactions.cache;
+      const thumbsUp = reactions.find((reaction) => reaction.emoji.name === '👍');
+      const thumbsDown = reactions.find((reaction) => reaction.emoji.name === '👎');
+      if (thumbsUp.count > thumbsDown.count) {
+        const name: string = message.content.split(' ')[2];
+        download(name, '1.16.5');
+      }
+    });
+  });
+}
+
+function handleLink(msg: any): void {
   const link: string = msg.content.toLowerCase();
   if (!link.startsWith('https://www.curseforge.com/minecraft/mc-mods/')) {
     msg.author.send('That is not a curseforge link.');
@@ -56,8 +78,8 @@ function handleLink(msg: any): null {
   const { channel } = msg;
 
   channel.send(`<@${author}> requested ${mod} (${link}). Vote below.`)
-    .then((msg) => {
-      msg.react('👍');
-      msg.react('👎');
+    .then((message) => {
+      message.react('👍');
+      message.react('👎');
     }).catch(console.error);
 }
